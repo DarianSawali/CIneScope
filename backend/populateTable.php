a8e6f1a0546d499ccf556de473df0c7a67dec1da
MichaelTj02 <mtjokrowardojo@gmail.com>
2025-04-07 16:08:16 -0700

    Update populateTable.php

--- backend/populateTable.php
+++ backend/populateTable.php
@@ -1,12 +1,17 @@
 <?php
+// populate movie table using exisiting database
+
 require_once "db.php";
 
+// open data in directory
 $csvFile = fopen("./data/cleaned_imdb_2024.csv", "r");
 fgetcsv($csvFile); 
 
+// prepare sql statement
 $stmt = $conn->prepare("INSERT INTO movies (title, cast, language, imdb_url, genre, overview, release_date, runtime) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssssi", $title, $cast, $language, $imdb_url, $genre, $overview, $release_date, $runtime);
 
+// insert data
 while (($row = fgetcsv($csvFile)) !== FALSE) {
     $imdb_url = $row[0];
     $title = $row[1]; 
@@ -20,6 +25,7 @@ while (($row = fgetcsv($csvFile)) !== FALSE) {
     $stmt->execute();
 }
 
+// close connection
 $stmt->close();
 $conn->close();
 fclose($csvFile);
