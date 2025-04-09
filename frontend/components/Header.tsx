a6a420a5369eb82d11fc18224bd1253993239b8b
Darian Sawali <darianaxel@gmail.com>
2025-04-08 21:09:30 -0700

    header comments

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -10,11 +10,13 @@ export default function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
 
+  // stores user login in localStorage
   useEffect(() => {
     const userId = localStorage.getItem('user_id')
     setIsLoggedIn(!!userId)
   }, [])
 
+  // handles user logout
   const handleLogout = () => {
     localStorage.removeItem('user_id')
     setIsLoggedIn(false)
@@ -24,14 +26,13 @@ export default function Header() {
   return (
     <header className="bg-gradient-to-r from-black/40 to-black/20 backdrop-blur-md p-4 mb-6 py-8 shadow-2xl shadow-indigo-950 border-b border-white/10">
       <div className="max-w-[100rem] mx-auto flex justify-between items-center px-4">
-        {/* Logo */}
+
         <Link href="/" className="flex items-center gap-1 text-white">
           <h1 className="text-2xl font-bold text-white hover:text-transparent bg-clip-text hover:bg-gradient-to-r hover:from-green-600 hover:to-cyan-600 transition duration-300">
             CINESCOPE
           </h1>
         </Link>
 
-        {/* Mobile Menu Icon */}
         <div className="md:hidden">
           <button
             onClick={() => setMenuOpen(!menuOpen)}
@@ -41,7 +42,6 @@ export default function Header() {
           </button>
         </div>
 
-        {/* Desktop Nav */}
         <nav className="hidden md:flex items-center gap-6">
           <Link href="/" className="flex items-center gap-1 text-white">
             <GoHome size={25} />
@@ -71,6 +71,7 @@ export default function Header() {
         </nav>
       </div>
 
+      {/* mobile menu open */}
       {menuOpen && (
         <div className="md:hidden mt-4 px-4 space-y-4">
           <Link href="/" onClick={() => setMenuOpen(false)} className="block text-white">
